a1af91584e7a31245521bac1f4d058484edbd6ab
HRx88 <S10262513@connect.np.edu.sg>
2024-04-24 01:36:50 +0800

    Week 1 Practical Completed

---
+++ Week 1/Practical 1/PathInfo.js
@@ -0,0 +1,7 @@
+const path = require("node:path");
+
+const notes = "/users/joe/notes.txt";
+
+console.log(path.dirname(notes)); // /users/joe
+console.log(path.basename(notes)); // notes.txt
+console.log(path.extname(notes)); // .txt
